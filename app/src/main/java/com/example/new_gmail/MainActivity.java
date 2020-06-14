3aee80fcdd1ac90bb79a0ba1a01bd8677ddd3f12
Deepak681 <47095705+Deepak681@users.noreply.github.com>
2020-06-14 14:53:19 +0530

    Some changes

--- app/src/main/java/com/example/new_gmail/MainActivity.java
+++ app/src/main/java/com/example/new_gmail/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
         signInButton = findViewById( R.id.sign_in_button );
+        
         signInButton.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick(View view) {
